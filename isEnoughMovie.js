9db71d22c24e82a36165d400cd7a532b5c0ed3cb
Andrew <arhorn@smcm.edu>
2019-03-16 00:09:40 -0500

    added O(n) solution with hash table

--- isEnoughMovie.js
+++ isEnoughMovie.js
@@ -1,17 +1,10 @@
 const isEnoughMovie = (flightLength, movieLengths) => {
-  movieLengths.sort();
-  let leftIndex = 0;
-  let rightIndex = movieLengths.length - 1;
-  while (leftIndex < rightIndex) {
-    const left = movieLengths[leftIndex];
-    const right = movieLengths[rightIndex];
-    if (left + right === flightLength) {
+  const dictOfTrues = {};
+  for (let movie of movieLengths) {
+    if (dictOfTrues[movie]) {
       return true;
-    } else if (left + right < flightLength) {
-      leftIndex++;
-    } else {
-      rightIndex++;
     }
+    dictOfTrues[flightLength - movie] = true;
   }
   return false;
 };
@@ -22,5 +15,5 @@ const flightLength = 335;
 let movieLengths = [88, 96, 107, 116, 125, 134];
 assert.equal(isEnoughMovie(flightLength, movieLengths), false, "test 1");
 movieLengths = [167, 168];
-assert.equal(isEnoughMovie(flightLength, movieLengths), true, "test 1");
+assert.equal(isEnoughMovie(flightLength, movieLengths), true, "test 2");
 console.log("all tests passed");
