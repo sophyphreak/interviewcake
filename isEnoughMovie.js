3f6e6521f614b6f91fc579d5258972b0c95e6bb5
Andrew <arhorn@smcm.edu>
2019-03-16 00:14:25 -0500

    added test with half the flightLength to make sure test still returns false

--- isEnoughMovie.js
+++ isEnoughMovie.js
@@ -12,7 +12,7 @@ const isEnoughMovie = (flightLength, movieLengths) => {
 const assert = require("assert");
 
 const flightLength = 335;
-let movieLengths = [88, 96, 107, 116, 125, 134];
+let movieLengths = [88, 96, 107, 167.5, 116, 125, 134];
 assert.equal(isEnoughMovie(flightLength, movieLengths), false, "test 1");
 movieLengths = [167, 168];
 assert.equal(isEnoughMovie(flightLength, movieLengths), true, "test 2");
