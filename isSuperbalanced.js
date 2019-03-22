2fe302287cb56dd7866d950ea67b27c74a6bfdeb
Andrew <arhorn@smcm.edu>
2019-03-21 22:54:29 -0500

    initialized superbalanced problem

---
+++ isSuperbalanced.js
@@ -0,0 +1,25 @@
+class BinaryTreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insertLeft(value) {
+    this.left = new BinaryTreeNode(value);
+    return this.left;
+  }
+
+  insertRight(value) {
+    this.right = new BinaryTreeNode(value);
+    return this.right;
+  }
+}
+
+const isSuperbalanced = root => {};
+
+const assert = require("assert");
+
+const root = "asfdsf";
+
+assert.ok(isSuperbalanced(root), "test 1");
